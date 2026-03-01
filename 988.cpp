d17319b90fa0058789045dcb423dc3c8cad29599
Tushar R C Raja <startushar.2004@gmail.com>
2026-03-01 20:25:59 +0530

    edited 988.cpp

--- 988.cpp
+++ 988.cpp
@@ -78,3 +78,84 @@ void f(TreeNode* root){
         
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+vector<string> arr;
+
+
+deque<char> s;
+
+vector<char> alp;
+
+void f(TreeNode* root){
+    if(root==nullptr){
+        return ;
+    }
+
+    if(root->left==nullptr and root->right==nullptr){
+
+         s.push_front(alp[root->val]);
+         string st="";
+         for(auto x:s){
+            st+=x;
+         }
+         arr.push_back(st);
+         s.pop_front();
+
+
+        
+        return;
+    }
+
+    s.push_front(alp[root->val]);
+
+
+
+    if(root->left){
+        f(root->left);
+    }
+
+    if(root->right){
+        f(root->right);
+    }
+
+    s.pop_front();
+
+
+}
+    string smallestFromLeaf(TreeNode* root) {
+
+        for(char x='a';x<='z';x++){
+            alp.push_back(x);
+        }
+
+
+        f(root);
+
+        // for(auto &x:arr){
+        //     reverse(x.begin(),x.end());
+        // }
+
+        sort(arr.begin(),arr.end());
+
+        return arr[0];
+
+
+        
+        
+    }
+};
